32a44ed8ca899fa32beb02a43b80911c6a639fbe
Glavyanov <dimitarglavianov@gmail.com>
2022-11-26 10:56:44 +0200

    Add user id extension

---
+++ CarHire/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,16 @@
+﻿namespace CarHire.Extensions
+{
+    using System.Security.Claims;
+    public static class ClaimsPrincipalExtension
+    {
+        /// <summary>
+        /// Custom extension on ClaimsPrincipal
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>string user Id</returns>
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}

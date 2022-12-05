8be999e509472c9ad95143ced50c0902a0881324
Glavyanov <dimitarglavianov@gmail.com>
2022-12-05 22:18:37 +0200

    Added Management Area

---
+++ CarHire/Areas/Management/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿namespace CarHire.Areas.Management.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using static CarHire.Infrastructure.Data.ValidationConstants.RolesConstants;
+
+    [Authorize(Roles = Manager)]
+    [Area("Management")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
---
+++ CarHire/Areas/Management/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿namespace CarHire.Areas.Management.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : BaseController
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
